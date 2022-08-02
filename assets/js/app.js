2ecfb207c1b368f00835bf9229fef52a92c3c378
rasouli-git <mahanrasoulii@yahoo.com>
2022-08-02 12:52:16 +0430

    projects

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+// start header
+const headerTag = document.querySelector(".header");
+const hamburgerMenu = document.querySelector(".header__humburger");
+
+hamburgerMenu.addEventListener("click", () => {
+  headerTag.classList.toggle("active");
+});
+// end header
+
+// start main
+
+// start video slide
+const speakerDiv = document.querySelector(".video-slide__mute");
+const videoDiv = document.querySelector(".video");
+const videoContent = document.querySelector(".video-slide__content");
+
+speakerDiv.addEventListener("click", () => {
+  speakerDiv.classList.toggle("active");
+
+  if (videoDiv.hasAttribute("muted")) {
+    videoDiv.removeAttribute("muted");
+  } else {
+    videoDiv.setAttribute("muted", "");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    videoContent.style.display = "none";
+    console.log("ok");
+  }, 7000);
+});
+// end video slide
+// end main
+
+// banner
+
+const bannerLeft = document.querySelector(".banner-fixed__left");
+const bannerDiv = document.querySelector(".banner-fixed");
+
+bannerLeft.addEventListener("click", () => {
+  bannerDiv.classList.toggle("active");
+});
